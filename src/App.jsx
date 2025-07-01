2d1269abe1456ae712cd3e9fe76071a65bc5f5c8
user-name-c <ivandgr_16@hotmail.com>
2025-07-01 15:09:30 -0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// https://github.com/adrianhajdin/react-movies/tree/main
+
+const App = () => {
+  return (
+    <div> 
+      <h1 class="text-3xl font-bold underline">
+    Hello world!
+   </h1>
+  </div>
+  )
+}
+
+export default App
