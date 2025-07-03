82126dfc8786c54d1fc79dba61dc55b0e9c238ee
user-name-c <ivandgr_16@hotmail.com>
2025-07-03 09:44:36 -0600

    add basic search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from './components/Search'
 
 // https://github.com/adrianhajdin/react-movies/tree/main
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('batman');
   return (
     <main>
       <div className='pattern'/>
@@ -13,7 +15,7 @@ const App = () => {
           Find <span className='text-gradient'>Movies</span> You'll enjoy Without the Hassle
         </h1>
       </header>
-      <p>Search</p>
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
   </div>
     </main>
   )
